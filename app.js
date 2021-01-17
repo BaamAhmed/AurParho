697043d349e936feb71f3e1c0bb1db936d2e1301
unknown <baamahmed03@gmail.com>
2021-01-17 15:51:50 +0500

    v1.2 crisis averted

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ let express 		= require("express"),
 
 //DATABASE CONFIG========================================
 // mongoose.connect("mongodb://localhost:27017/yelp_camp",
-mongoose.connect('mongodb://localhost:27017/new_thing',
-// mongoose.connect('mongodb+srv://BaamAhmed:aurparho@aurparho.seqhs.mongodb.net/aurparho?retryWrites=true&w=majority',
+// mongoose.connect('mongodb://localhost:27017/new_thing',
+mongoose.connect('mongodb+srv://BaamAhmed:aurparho@aurparho.seqhs.mongodb.net/aurparho?retryWrites=true&w=majority',
 	{
 	useNewUrlParser: true,
 	useUnifiedTopology: true
